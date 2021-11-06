a25810438e13cd435b4fd19c4a5eaa0671631714
Rusty-Song <andrew.q.song@gmail.com>
2021-11-06 19:27:35 -0400

    Fixed a bug: no_3 is changed to real_answer.

--- math_facts_v1.py
+++ math_facts_v1.py
@@ -1,11 +1,11 @@
 import random
 
-how_many_times=5
+how_many_times=50
 
 right=0
 for i in range(1,how_many_times+1):
     no_1=random.randint(0,100)
-    no_2=random.randint(0,10)
+    no_2=random.randint(0,20)
     real_answer=no_1+no_2
     print("Question {}: {}+{}=?".format(i,no_1,no_2))
     answer=input()
@@ -14,5 +14,5 @@ for i in range(1,how_many_times+1):
         print("correct")
         right=right+1
     else:
-        print("no it should be {}".format(no_3))
+        print("no it should be {}".format(real_answer))
 print("you got {} / {}".format(right,how_many_times))
