e0d07da45de50ecd557074713c67550286df7fa5
Rusty-Song <andrew.q.song@gmail.com>
2021-11-14 12:11:06 -0500

    Fixed the bug that the program crashes when a non-integer is typed, for example, the enter key is pressed twice accidentally.

--- math_facts_v1.py
+++ math_facts_v1.py
@@ -4,15 +4,21 @@ how_many_times=50
 
 right=0
 for i in range(1,how_many_times+1):
-    no_1=random.randint(0,100)
-    no_2=random.randint(0,20)
-    real_answer=no_1+no_2
-    print("Question {}: {}+{}=?".format(i,no_1,no_2))
-    answer=input()
-    number=int(answer)
+    no_1=random.randint(0,10)
+    no_2=random.randint(3,3)
+    real_answer=no_1*no_2
+    is_an_integer=False
+    while(is_an_integer==False):
+        print("Question {}: {}x{}=?".format(i,no_1,no_2))
+        answer=input()
+        try:
+            number=int(answer)
+            is_an_integer=True
+        except:
+            print('Not an integer. Please try again.')
     if(number==real_answer):
         print("correct")
         right=right+1
     else:
-        print("no it should be {}".format(real_answer))
+        print("No it should be {}".format(real_answer))
 print("you got {} / {}".format(right,how_many_times))
