75529a8f2390dd9964ed617ce7c9078e693ca0c5
Rusty-Song <andrew.q.song@gmail.com>
2021-11-28 20:48:11 -0500

    Optimized a block of codes into functions, so it is easier to understand.

--- math_facts_v1.py
+++ math_facts_v1.py
@@ -1,5 +1,23 @@
 import random
 
+def get_an_integer_input_or_none():
+    input_string=input()
+    try:
+        number=int(input_string)
+    except:
+        number=None
+    return number
+
+def must_get_an_integer_input():
+    is_an_integer=False
+    while(is_an_integer==False):
+        integer_or_none=get_an_integer_input_or_none()
+        if (integer_or_none is not None):
+            is_an_integer=True
+        else:
+            print('\tNot an integer. Please try again.')
+    return integer_or_none
+
 how_many_times=50
 
 right=0
@@ -7,18 +25,11 @@ for i in range(1,how_many_times+1):
     no_1=random.randint(0,10)
     no_2=random.randint(3,3)
     real_answer=no_1*no_2
-    is_an_integer=False
-    while(is_an_integer==False):
-        print("Question {}: {}x{}=?".format(i,no_1,no_2))
-        answer=input()
-        try:
-            number=int(answer)
-            is_an_integer=True
-        except:
-            print('Not an integer. Please try again.')
-    if(number==real_answer):
-        print("correct")
+    print("Question {}: {}x{}=?".format(i,no_1,no_2))
+    answer=must_get_an_integer_input()
+    if(answer==real_answer):
+        print("*^-^*correct*^-^*")
         right=right+1
     else:
-        print("No it should be {}".format(real_answer))
+        print("\tNo it should be {}".format(real_answer))
 print("you got {} / {}".format(right,how_many_times))
